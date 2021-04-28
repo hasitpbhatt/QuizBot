bb8969e7d4936fec2c5d1e605b4acaeca2274ffd
Hasit Bhatt <hasit.p.bhatt@gmail.com>
2021-04-28 22:24:04 +0000

    Minor changes to tag Role every 2 minutes in the same chat, and some improvements for captcha

--- image_utils.py
+++ image_utils.py
@@ -1,10 +1,14 @@
 import requests
-from PIL import Image
+from PIL import Image, ImageFilter, ImageOps
 from io import BytesIO
+import pytesseract
+import os
+
 
 def get_image(url):
     r = requests.get(url)
     return Image.open(BytesIO(r.content))
+
 
 def get_image_from_shoob(url):
     i = get_image(url)
@@ -22,19 +26,80 @@ def process(img):
     for i in range(img.size[0]):
         cnt = 0
         for j in range(img.size[1]):
-            if p[i, j] == 17:
+            if p[i, j] == 0:
                 cnt += 1
             if cnt > 2:
                 break
         if cnt >= 3:
             r = max(i, r)
             l = min(i, l)
+        # print(i,j,p[i,j])
     return img.crop((max(l - 5, 0), 0, min(r + 5, img.size[0]), img.size[1]))
 
+def process2(img):
+  img2 = ImageOps.invert(img)
+  k = img2.filter(ImageFilter.ModeFilter(size=5))
+  l = k.filter(ImageFilter.GaussianBlur(radius=9))
+  m = Image.composite(img2,k,l).convert('L')
+  return ImageOps.invert(m)
+
+def init_tesseract():
+  os.system('install-pkg tesseract-ocr')
+  os.environ['TESSDATA_PREFIX'] = "/home/runner/QuizBot/tessdata"
 
 if __name__ == '__main__':
-    img = get_image_file('826498439936344115.png').convert('L')
-    process(img).save('zzz.png')
+    img = get_image_file('826498439936344115.png').convert('1').convert('L')
+    i = process(img)
+    img2 = ImageOps.invert(i)
+    # img2 = i
+    # img2 = invert(i)
+    # i = j.filter(ImageFilter.MinFilter).filter(ImageFilter.MaxFilter).filter(ImageFilter.MaxFilter).filter(ImageFilter.MinFilter)
+    # print(pytesseract.image_to_string(j))
+    # j = j.filter(ImageFilter.MinFilter)
+    # print(pytesseract.image_to_string(j))
+    # j = j.filter(ImageFilter.MaxFilter)
+    # print(pytesseract.image_to_string(j))
+    # j = j.filter(ImageFilter.MaxFilter)
+    # print(pytesseract.image_to_string(j))
+    # j = j.filter(ImageFilter.MinFilter)
+    # print(pytesseract.image_to_string(j))
+    # j.save('zzz.png')
+    # j = i
+    # j = j.filter(ImageFilter.MaxFilter)
+    # j = img2.filter(ImageFilter.ModeFilter(size=3))
+    k = img2.filter(ImageFilter.ModeFilter(size=5))
+    # l = img2.filter(ImageFilter.ModeFilter(size=7))
+    # .filter(
+    #     ImageFilter.Kernel((3, 3), (3, 0, 3, 0, 1, 0, 3, 0, 3), 13, 0))
+    # .filter(ImageFilter.Kernel((5, 5),
+    # (1/10, 1/10, 1/10, 1/10, 1/10,
+    # 0, 0, 0, 0,0,
+    # 0, 0, 0, 0,0,
+    # 0, 0, 0, 0,0,
+    # 1/10, 1/10, 1/10, 1/10, 1/10), 1, 0))
+    # .filter(ImageFilter.Kernel((3, 3),(0,0,0,1/6, 1/6, 1/6, 1/6, 1/6, 1/6), 1, 0))
+    # .filter(ImageFilter.MedianFilter(size=3)).filter(ImageFilter.MedianFilter(size=3)).filter(ImageFilter.MedianFilter(size=3))
+    # .filter(ImageFilter.MinFilter)
+    # print(pytesseract.image_to_string(img2))
+    # img2 = j
+    # img2 = Image.blend(j,k,0.5)
+    # img3 = Image.blend(k,l,0.5)
+    # img4 = Image.blend(img2,img3,0.5)
+    # img5 = invert(img4)
+    # img5 = invert(k)
+    # k = k.filter(ImageFilter.BoxBlur(2))
+    l = k.filter(ImageFilter.GaussianBlur(radius=9))
+    
+
+    
+    # m = ImageOps.invert(k)
+    m = Image.composite(img2,k,l).convert('L')
+    z = ImageOps.invert(m)
+    z.save('zzz.png')
+    # j = i.filter(ImageFilter.MinFilter).filter(ImageFilter.MaxFilter).filter(ImageFilter.MaxFilter).filter(ImageFilter.MinFilter)
+    print('OCR')
+    print(pytesseract.image_to_string(z))
+    # j.save('zzz1.png')
 
 
 def image_to_bin(im):
--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import discord
 import asyncio
 from keep_alive import keep_alive
 import image_utils as iu
-# install-pkg 
 import pytesseract
+import time
 
 client = discord.Client()
+
+mp = dict()
 
 
 @client.event
@@ -23,15 +25,21 @@ async def on_message(message):
         claim = False
         for x in message.embeds:
             y = x.to_dict()
-            if 'image' in y:
+            if 'image' in y and message.channel.id == 836759068755361832:
                 print(y['image']['url'])
-                im = iu.get_image_from_shoob(y['image']['url']).convert('L')
-                im.save('a.png')
+                im = iu.get_image_from_shoob(y['image']['url']).convert('1').convert('L')
                 filename = '{}.png'.format(message.channel.id)
-                iu.process(im).save(filename)
+                kk = iu.process(im)
+                zz = iu.process2(kk)
+                zz.save(filename)
                 await message.channel.send(file=discord.File(filename))
-                ch = message.channel
-                await read_image_and_send(ch, filename)
+                s = time.time()
+                txt = pytesseract.image_to_string(zz)
+                txt = txt.replace(' ','').replace('\n','').replace('\t','')
+                print('txt',txt)
+                await message.channel.send(
+                '#### ' + txt + ' ####')
+                print('took', time.time()-s)
                 return
 
             elif 'got the card!' in y.get('description'):
@@ -39,29 +47,26 @@ async def on_message(message):
                 print(y['description'])
 
         if claim:
+            mp[message.channel.id] = time.time()
             await asyncio.sleep(120)
-            await message.channel.send('Kalpana was killed')
-
-    if not message.author.bot and message.content == 'read this' and len(message.attachments) > 0:
-        print('reading')
-        y = message.attachments[0]
-        im = iu.get_image(y)
-        filename = '{}_{}.png'.format(message.author.id, message.channel.id)
-        im.save(filename)
-        ch = message.channel
-        await read_image_and_send(ch, filename)
-
-async def read_image_and_send(channel, filename):
-    txt = pytesseract.image_to_string(filename)
-    print('got',txt)
-    if txt and txt.strip() != '':
-        print('txt',txt)
-        await channel.send('-----\n' + txt + '\n-----\n')
-    else:
-        await channel.send('-----\n' + "Kuchh samajh nahi aaya" + '\n-----\n')
+            y = time.time()
+            if y-mp[message.channel.id] >= 120:
+              out_channel = message.channel
+              # try:
+              #   for ch in message.guild.channels:
+              #       if 'shoob-reminder' in ch.name:
+              #           print("found",ch)
+              #           out_channel = client.get_channel(ch.id)
+              # except:
+              #   print("error occurred")
+              # await out_channel.send('@{} feels lonely!!! <#{}>'.format(message.author.id,message.channel.id))
+              role = discord.utils.find(
+        lambda r: r.name == 'T1' or r.name == 'Ghajini', message.guild.roles)
+              if role:
+                await out_channel.send('<@&{}> Shoob remembers you!!!'.format(role.id))
 
 def run_server():
-    os.environ['TESSDATA_PREFIX'] = "/home/runner/QuizBot/tessdata"
+    iu.init_tesseract()
     keep_alive()
     client.run(os.environ['token'])
 
